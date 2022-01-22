ae38766c8c6dca15e3f160123bbb9fce41f75b18
sxhoji <sofasky17@gmail.com>
2022-01-22 07:21:53 -0800

    added new changes.

--- javaScript/moviesProject.js
+++ javaScript/moviesProject.js
@@ -1,18 +1,21 @@
-"use strict";
+'use strict';
 
 let NumberOfFilms;
 
 function start() {
-  NumberOfFilms = +prompt("How many movies did you watch?", "");
+  NumberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
 
   while (
-    NumberOfFilms == "" ||
+    NumberOfFilms == '' ||
     NumberOfFilms == null ||
     NumberOfFilms == isNaN(NumberOfFilms)
-  )
-    NumberOfFilms = +prompt("How many movies did you watch?", "");
+  ) {
+    NumberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+  }
 }
- 
+
+start();
+
 const personalMovieDB = {
   count: NumberOfFilms,
   movies: {},
@@ -21,35 +24,50 @@ const personalMovieDB = {
   privat: false,
 };
 
-let a = prompt("One of watched movies?");
-let b = prompt("How much you would rate it?");
-let c = prompt("One of watched movies?");
-let d = prompt("How much you would rate it?");
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один з переглянутих фільмів?', ''),
+      b = prompt('На сквільки оціните його?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-for (let i = 0; i < 2; i++) {
-  a = prompt("One of watched movies?");
-  b = prompt("How much you would rate it?");
-
-  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-    personalMovieDB.movies[a] = b;
-    console.log("done");
-  } else {
-    console.log("error");
-    i--;
-  }
-
-  if (personalMovieDB.count < 10) {
-    console.log("Why that little?");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Classic");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("wow");
-  } else {
-    console.log("error");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMovieDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
   }
 }
 
-console.log(personalMovieDB);
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Переглянуто доволі мало фільмів');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Ви класичний глядач');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Ви кіноман');
+  } else {
+    console.log('Сталася помилка');
+  }
+}
+
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(
+      `Ваш улюблений жанр під номером ${i}`
+    );
+  }
+}
+writeYourGenres();
--- javaScript/script.js
+++ javaScript/script.js
@@ -1,51 +1,48 @@
-let num = 50;
+let num = 51;
 
 switch (num) {
-  case 49: 
-  console.log('You good?');
-  break;
+  case 50:
+    console.log('You good?');
+    break;
   case 100:
     console.log('Nah');
     break;
-    case 51:
-      console.log('yaaas'); 
-      break;
-      default: 
-      console.log('you are stupid');
-      break;
+  case 51:
+    console.log('yaaas');
+    break;
+  default:
+    console.log('you are stupid');
+    break;
 }
-
 
 let box = 50;
 
-while (box <= 55) {
-console.log (box);
-box++;
-}
+// while (box <= 55) {
+//   console.log(box);
+//   box++;
+// }
 
 do {
-console.log(box);
-box++;
-}
-while (box < 55);
+  console.log(box);
+  box++;
+} while (box < 55);
 
 let oj = 7;
 
 function showFirstMessage(text) {
-   console.log(text);
-   oj = 8;
+  console.log(text);
+  oj = 8;
 }
 
 showFirstMessage('lol');
 
 function calc(a, b) {
-  return(a + b);
+  return a + b;
 }
- 
+
 console.log(calc(4, 8));
 console.log(calc(7, 9));
 console.log(calc(2, 0));
-
 
 function ret() {
   let num = 50;
@@ -55,15 +52,15 @@ function ret() {
 const anotherNum = ret();
 console.log(anotherNum);
 
-let something = function() {
-  console.log("Hello");
+let something = function () {
+  console.log('Hello');
 };
 
 something();
 
 calc = (a, b) => {
   console.log('2');
-  return (a + b);
+  return a + b;
 };
 calc();
 
@@ -74,7 +71,7 @@ console.log(str.toUpperCase());
 const logg = 'Hello world';
 console.log(logg.slice(6, 10));
 
-const fruit = "some fruit";
+const fruit = 'some fruit';
 console.log(fruit.indexOf('t'));
 
 let smthng = 2.5;
