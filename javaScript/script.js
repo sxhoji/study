f8f203d53a1f0de9f0d5e01bf4529aff5751c6e8
sxhoji <sofasky17@gmail.com>
2022-01-31 08:46:55 -0800

    tested some objects

--- javaScript/script.js
+++ javaScript/script.js
@@ -105,3 +105,39 @@ function done() {
 }
 
 learn('JS', done);
+
+
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colours: {
+    border: 'black',
+    background: 'red'
+  },
+  makeTest: function() {
+    console.log('test');
+  }
+};
+
+options.makeTest();
+
+const {border, background} = options.colours;
+console.log(border);
+
+console.log(Object.keys(options['colours']).length);
+
+// console.log(options.name);
+
+// delete options.name;
+// console.log(options);
+
+for (let key in options) {
+  if (typeof(options[key]) === 'object'){
+    for (let i in options[key]) {
+      console.log(`властивість ${key} має значення ${options[key][i]}`);
+    }
+  } else {
+    console.log(`властивість ${key} має значення ${options[key]}`);
+  }
+};
