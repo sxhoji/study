1e06107a65f58e83e71584bb701dd4d05162f79b
sxhoji <sofasky17@gmail.com>
2022-03-09 23:05:47 +0200

    new material

--- javaScript/script2.0.js
+++ javaScript/script2.0.js
@@ -31,3 +31,55 @@ let str = prompt('', '');
 const products = str.split(', ');
 products.sort();
 console.log(products.join('; '));
+
+// let a = 5,
+//     b = a;
+
+// b = b = 5;
+
+// console.log(b);
+// console.log(a);
+
+// const poop = {
+//   a: 5,
+//   b: 1
+// };
+
+// const copy = poop;
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(poop);
+
+function double(mainObj) {
+  let copyObj = {};
+
+  let key;
+  for (key in mainObj) {
+    copyObj[key] = mainObj[key];
+  }
+
+  return copyObj;
+}
+
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4,
+  },
+};
+
+const newNumbers = double(numbers);
+
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+  d: 17,
+  e: 20,
+};
+
+console.log(Object.assign(numbers, add));
